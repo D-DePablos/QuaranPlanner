473697f9b738045be1e7d1f4a0e563fa3603ba97
Diego de Pablos <d.depablosaguero@gmail.com>
2020-04-25 14:21:58 +0100

    CORS new test

--- backend_django/QuarenPlanner/views.py
+++ backend_django/QuarenPlanner/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render
-
 # Create your views here.
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import Group
 from rest_framework import viewsets
 from rest_framework import permissions
 from QuarenPlanner.serializers import UserSerializer, GroupSerializer, EventSerializer
-from django.shortcuts import render
 from QuarenPlanner.models import Event
+from django.contrib.auth.models import User
 
 
 class UserViewSet(viewsets.ModelViewSet):
